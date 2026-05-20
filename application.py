0c6cf00ace0d0d0da3f3b00f34e554f404ea2c34
Oleh Zhuravel <lordlive74@gmail.com>
2026-05-20 20:09:03 +0300

    another deploy

--- application.py
+++ application.py
@@ -1,14 +1,14 @@
 from flask import Flask
 
-# Elastic Beanstalk шукає саме змінну з назвою "application"
+# Elastic Beanstalk looks for exactly the variable with the name "application"
 application = Flask(__name__)
 
 
 @application.route('/')
 def index():
-    return "<h1>Flask App is running on AWS Elastic Beanstalk!</h1>"
+    return "<h1>Hello! Automated CI/CD works perfectly!</h1>"
 
 
 if __name__ == "__main__":
-    # Локальний запуск (AWS ігнорує цей блок і запускає через WSGI)
+    # Local development (AWS ignores this block and runs through WSGI)
     application.run(host="0.0.0.0", port=5000)
