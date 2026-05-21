dd821b2f7f69d8d421aa18577d3ed5293d06b09d
Oleh Zhuravel <lordlive74@gmail.com>
2026-05-21 13:07:20 +0300

    test lab

--- application.py
+++ application.py
@@ -1,14 +1,8 @@
 from flask import Flask
 
-# Elastic Beanstalk looks for exactly the variable with the name "application"
 application = Flask(__name__)
 
 
 @application.route('/')
 def index():
-    return "<h1>Hello! Automated CI/CD works perfectly!</h1>"
-
-
-if __name__ == "__main__":
-    # Local development (AWS ignores this block and runs through WSGI)
-    application.run(host="0.0.0.0", port=5000)
+    return "Hello from u3vaje75 Elastic Beanstalk CI-CD"
